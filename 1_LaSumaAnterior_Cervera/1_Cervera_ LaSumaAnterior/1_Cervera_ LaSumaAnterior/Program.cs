affcb2d41a383dc382fd7c48cb122f06db1e79a1
LourdesCervera <168189411+LourdesCervera@users.noreply.github.com>
2024-06-05 16:26:33 -0300

    subo 0-4

---
+++ 1_LaSumaAnterior_Cervera/1_Cervera_ LaSumaAnterior/1_Cervera_ LaSumaAnterior/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1_Cervera__LaSumaAnterior
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1;
+            int num2;
+            int num3;
+            int suma;
+            Console.WriteLine("ingrese un numero: ");
+            num1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("ingrese otro numero: ");
+            num2 = int.Parse(Console.ReadLine());
+            Console.WriteLine("ingrese otro numero: ");
+            num3 = int.Parse(Console.ReadLine());
+            Console.WriteLine("suma: " + (num1 + num2 + num3));
+            suma = int.Parse(Console.ReadLine());
+            Console.ReadKey();
+
+        }
+    }
+}
