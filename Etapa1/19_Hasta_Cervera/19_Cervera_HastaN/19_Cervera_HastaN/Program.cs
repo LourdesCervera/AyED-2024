a67a46a5adbe41c2a862f663bab02ebbd72d4b11
LourdesCervera <168189411+LourdesCervera@users.noreply.github.com>
2024-06-05 16:51:46 -0300

    subo 19

---
+++ Etapa1/19_Hasta_Cervera/19_Cervera_HastaN/19_Cervera_HastaN/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _19_Cervera_HastaN
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N;
+            Console.WriteLine("Ingrese un numero:");
+            N = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("num pares: ");
+
+            for (int cont = 1; cont <= N; cont++)
+            {
+                if (cont % 2 == 0)
+                {
+                    Console.WriteLine(cont);
+                }
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
