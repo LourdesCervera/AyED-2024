4adf7b3da7fa1ee57070b3a7e5a6489734c40b6a
LourdesCervera <168189411+LourdesCervera@users.noreply.github.com>
2024-06-05 16:35:50 -0300

    subo 0-5

---
+++ Etapa1/2_BienvenidoACiudad_Cervera/2_Cervera_BienvenidoACiu/2_Cervera_BienvenidoACiu/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2_Cervera_BienvenidoACiu
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String nombre;
+            String ciudad;
+            Console.WriteLine("Escriba un nombre ");
+            nombre = Console.ReadLine();
+            Console.WriteLine("Escriba una ciudad ");
+            ciudad = Console.ReadLine();
+            Console.WriteLine("Hola " + nombre + " Bienvenido a " + ciudad);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Etapa1/4_CentigradoKelvinFarenheit_Cervera/4_Cervera_CentigradosK/4_Cervera_CentigradosK/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4_Cervera_CentigradosK
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int celsius;
+            Console.WriteLine("Ponga una cantidad de grados para convertirlos");
+            celsius = int.Parse(Console.ReadLine());
+            Console.WriteLine("En kelvin son " + (celsius + 273));
+            Console.WriteLine("y en farenheit son " + (celsius * 18 / 10 + 32));
+            Console.ReadKey();
+        }
+    }
+}
