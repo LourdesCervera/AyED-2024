50584770faa5fe3632220c4722c913144eeec46f
LourdesCervera <168189411+LourdesCervera@users.noreply.github.com>
2024-08-07 15:58:53 -0300

    subo 10

---
+++ Etapa2/10_Cervera_MatrizAleatoria/10_Cervera_MatrizAleatoria/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Ponga la cantidad de filas");
+        int n = int.Parse(Console.ReadLine());
+        Console.WriteLine("Ponga la cantidad de columnas");
+        int m = int.Parse(Console.ReadLine());
+        Console.WriteLine("Cuanto es el maximo para generar numeros randoms en la matriz?(El minimo es 1");
+        int a = int.Parse(Console.ReadLine());
+
+        Random random = new Random();
+
+        int[,] matrix = new int[n, m];
+
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < m; j++)
+            {
+                matrix[i, j] = random.Next(1, a + 1);
+            }
+        }
+
+        Console.WriteLine("La matriz generada es:");
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < m; j++)
+            {
+                Console.Write(matrix[i, j] + "\t");
+            }
+            Console.WriteLine();
+        }
+        Console.ReadKey();
+    }
+}
