fc8ff8bf561bbde9131a47b82e4378dd309e1dac
LourdesCervera <168189411+LourdesCervera@users.noreply.github.com>
2024-10-30 15:17:33 -0300

    subo 13-14

---
+++ Etapa2/13_ObtenerDiagonalPrincipal_Cervera/13_Cervera_ObtenerDiagonalPrincia/13_Cervera_ObtenerDiagonalPrincia/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int n = 4;
+
+        Random random = new Random();
+        int[,] matriz = new int[n, n];
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                matriz[i, j] = random.Next(1, 100);
+            }
+        }
+
+        int[] diagonalPrincipal = new int[n];
+
+        for (int i = 0; i < n; i++)
+        {
+            diagonalPrincipal[i] = matriz[i, i];
+        }
+
+        Console.WriteLine("Matriz:");
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                Console.Write(matriz[i, j] + "\t");
+            }
+            Console.WriteLine();
+        }
+
+        Console.WriteLine(" Diagonal principal:");
+        for (int i = 0; i < n; i++)
+        {
+            Console.Write(diagonalPrincipal[i] + " ");
+        }
+        Console.ReadKey();
+    }
+}
---
+++ Etapa2/14_CalificacionesEstudiantiles_Cervera/14_Cervera_CalificacionesEstudia/14_Cervera_CalificacionesEstudia/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int n = 3;
+        string[,] estudiantes = new string[n, 3];
+
+        for (int i = 0; i < n; i++)
+        {
+            Console.WriteLine("Estudiante " + (i + 1) + ":");
+
+            Console.Write("Nombre: ");
+            estudiantes[i, 0] = Console.ReadLine();
+
+            Console.Write("Edad: ");
+            estudiantes[i, 1] = Console.ReadLine();
+
+            Console.Write("Calificación: ");
+            estudiantes[i, 2] = Console.ReadLine();
+        }
+
+        Console.WriteLine("Datos de los estudiantes:");
+        for (int i = 0; i < n; i++)
+        {
+            Console.WriteLine("Estudiante " + (i + 1) + ": Nombre: " + estudiantes[i, 0] + ", Edad: " + estudiantes[i, 1] + ", Calificación: " + estudiantes[i, 2]);
+        }
+        Console.ReadKey();
+    }
+}
